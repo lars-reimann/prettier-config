df71e41ae1ed04a948155a5f17110b8d3b31c5d0
Lars Reimann <mail@larsreimann.com>
2024-02-13 14:30:04 +0100

    feat: dedicated config for svelte projects (#12)
    * ci: remove MegaLinter
    
    * feat: dedicated config for svelte projects

---
+++ packages/svelte/README.md
@@ -0,0 +1,14 @@
+# Configuration for [Prettier](https://prettier.io/) in Svelte Projects
+
+To use this configuration:
+
+1. Run `npm install --save-dev @lars-reimann/prettier-config-svelte`.
+2. Include the following in your `package.json`:
+
+```
+{
+    ...
+    "prettier": "@lars-reimann/prettier-config-svelte",
+    ...
+}
+```
--- release.config.js
+++ release.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-    "branches": ["main"],
-    "plugins": [
-        ["@semantic-release/commit-analyzer", {
-            "preset": "conventionalcommits",
-        }],
-        ["@semantic-release/release-notes-generator", {
-            "preset": "conventionalcommits",
-        }],
-        "@semantic-release/npm",
-        "@semantic-release/github",
-    ]
+    'branches': ['main'],
+    'plugins': [
+        ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+        ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+        ['@semantic-release/npm', { pkgRoot: 'packages/base' }],
+        ['@semantic-release/npm', { pkgRoot: 'packages/svelte' }],
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                assets: [
+                    'package-lock.json',
+                    'packages/base/package.json',
+                    'packages/svelte/package.json',
+                ],
+            },
+        ],
+    ],
 };
